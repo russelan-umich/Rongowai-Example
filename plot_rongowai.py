3b6503215751bc8437d6f10df988cc085f240a40
Anthony Russel <53877205+russelan-umich@users.noreply.github.com>
2024-11-12 14:32:35 -0500

    Using SNR instead of reflectivity for plot

--- plot_rongowai.py
+++ plot_rongowai.py
@@ -26,7 +26,7 @@ import os
 # Initialize lists to accumulate data
 all_sp_lats = []
 all_sp_lons = []
-all_refl = []
+all_snr = []
 all_quality_flags1 = []
 all_ants = []
 
@@ -42,7 +42,7 @@ for file in files:
     all_sp_lons.extend(ds['sp_lon'].values.flatten())
 
     # Grab the data variables
-    all_refl.extend(ds['surface_reflectivity_peak'].values.flatten())
+    all_snr.extend(ds['ddm_snr'].values.flatten())
     all_ants.extend(ds['ddm_ant'].values.flatten())
     all_quality_flags1.extend(ds['quality_flags1'].values.flatten())
     
@@ -52,14 +52,14 @@ data = {
     'Latitude': all_sp_lats,
     'Longitude': all_sp_lons,
     'Quality Flags': all_quality_flags1,
-    'Reflectivity': all_refl ,
+    'SNR': all_snr ,
     'Antenna': all_ants
 }
 df = pd.DataFrame(data)
 
-# Remove all rows with a NaN for the Reflectivity or the 1st bit of the quality 
+# Remove all rows with a NaN for the SNR or the 1st bit of the quality 
 # flags is set
-df = df.dropna(subset=['Reflectivity'])
+df = df.dropna(subset=['SNR'])
 df = df[df['Quality Flags'] & 1 == 0]
 
 '''
@@ -83,7 +83,7 @@ fig.update_layout(mapbox_style='open-street-map',
         mapbox_center_lon=174.8860,  # Center the map on New Zealand
         mapbox_center_lat=-40.9006,
         mapbox_zoom=4, 
-        title='Rongowai Reflectivity on 2024-04-04')
+        title='Rongowai SNR on 2024-04-04 UTC')
 
 fig.add_trace(go.Scattermapbox(
             lon=df['Longitude'],
@@ -92,20 +92,20 @@ fig.add_trace(go.Scattermapbox(
             marker=dict(
                 size=8,
                 colorscale='viridis',
-                color=df['Reflectivity'],
+                color=df['SNR'],
                 cmin=0.0,
-                cmax=0.5,
+                cmax=10.0,
                 colorbar=dict(
-                    title='Reflectivity (db/meter^2)',
+                    title='Digital Delay Doppler Map (DDM) SNR)',
                     titleside='right',
                     len=0.75,
                     y=0.4,  # Set the y position of the colorbar
                     yanchor='middle'
                 )
             ),
-            name='Reflectivity',
+            name='SNR',
             customdata=list(zip(df['Longitude'], df['Latitude'])),
-            hovertemplate='Reflectivity: %{marker.color:.4f} db/meter^2<br>Latitude: %{customdata[1]:.2f}<br>Longitude: %{customdata[0]:.2f}<extra></extra>', 
+            hovertemplate='SNR: %{marker.color:.4f} <br>Latitude: %{customdata[1]:.2f}<br>Longitude: %{customdata[0]:.2f}<extra></extra>', 
             hoverinfo='text', # Show hover text
             showlegend=False
         ))
